ffe8d50e8c88c2eb3a6f190ad0e3e94e28da2cde
tony <a0921444212@gmail.com>
2023-02-24 23:58:45 +0800

    add a command to insatall vim and tmux

--- Linux/set_dev.sh
+++ Linux/set_dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#install editor
+sudo apt install vim tmux
+
 #install some coding tool
 sudo apt install build-essential git-core valgrind
 sudo apt install cppcheck clang-format aspell colordiff
