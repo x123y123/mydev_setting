7fd8ae7edf017f7a627b2754e34991bbe7e78833
tony <a0921444212@gmail.com>
2023-02-25 10:19:40 +0800

    add installing libc6-dbg command because valgrind will trace to glibc

--- Linux/set_dev.sh
+++ Linux/set_dev.sh
@@ -13,6 +13,9 @@ sudo apt install htop
 sudo apt install iotop
 sudo apt install sysstat
 
+#Valgrind is dynamic tracing, it will trace to the glibc
+sudo apt install libc6-dbg
+
 # set some env variable
 echo "export EDITOR=vim" >> ~/.bashrc
 git config --global core.editor vim
